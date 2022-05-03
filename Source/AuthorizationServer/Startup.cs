9b7cb75bc61af9ab583d363d5462264063e041c5
Lars Michael <lars.michael.dk@gmail.com>
2022-05-03 16:29:50 +0200

    Using PostgreSQL-provider

--- Source/AuthorizationServer/Startup.cs
+++ Source/AuthorizationServer/Startup.cs
@@ -1,124 +1,120 @@
-﻿namespace AuthorizationServer
+﻿namespace AuthorizationServer;
+
+using DHI.Services.Accounts;
+using DHI.Services.Authentication;
+using DHI.Services.Authorization;
+using DHI.Services.Logging;
+using DHI.Services.Mails;
+using DHI.Services.WebApiCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Polly;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Security.Claims;
+
+public class Startup
 {
-    using DHI.Services.Accounts;
-    using DHI.Services.Authentication;
-    using DHI.Services.Authorization;
-    using DHI.Services.Logging;
-    using DHI.Services.Mails;
-    using DHI.Services.WebApiCore;
-    using Microsoft.AspNetCore.Authentication.JwtBearer;
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.AspNetCore.Mvc.Versioning;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Hosting;
-    using Microsoft.IdentityModel.Tokens;
-    using Microsoft.OpenApi.Models;
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Converters;
-    using Polly;
-    using Swashbuckle.AspNetCore.SwaggerUI;
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Net.Http;
-    using System.Security.Claims;
-    using AccountRepository = DHI.Services.Security.WebApi.AccountRepository;
-    using MailTemplateRepository = DHI.Services.Security.WebApi.MailTemplateRepository;
-    using RefreshTokenRepository = DHI.Services.Security.WebApi.RefreshTokenRepository;
-    using UserGroupRepository = DHI.Services.Security.WebApi.UserGroupRepository;
-
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Authentication
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Tokens:Issuer"],
+                    ValidAudience = Configuration["Tokens:Audience"],
+                    IssuerSigningKey = RSA.BuildSigningKey(Configuration["Tokens:PublicRSAKey"].Resolve())
+                };
+            });
+
+        // Authorization
+        services.AddAuthorization(options =>
         {
-            Configuration = configuration;
-        }
+            options.AddPolicy("AdministratorsOnly", policy => policy.RequireClaim(ClaimTypes.GroupSid, "Administrators"));
+            options.AddPolicy("EditorsOnly", policy => policy.RequireClaim(ClaimTypes.GroupSid, "Editors"));
+        });
 
-        public IConfiguration Configuration { get; }
-
-        public void ConfigureServices(IServiceCollection services)
+        // API versioning
+        services.AddApiVersioning(options =>
         {
-            // Authentication
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true,
-                        ValidateIssuerSigningKey = true,
-                        ValidIssuer = Configuration["Tokens:Issuer"],
-                        ValidAudience = Configuration["Tokens:Audience"],
-                        IssuerSigningKey = RSA.BuildSigningKey(Configuration["Tokens:PublicRSAKey"].Resolve())
-                    };
-                });
+            options.ReportApiVersions = true;
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+            options.ApiVersionReader = ApiVersionReader.Combine(
+                new QueryStringApiVersionReader("api-version", "version", "ver"),
+                new HeaderApiVersionReader("api-version"));
+        });
 
-            // Authorization
-            services.AddAuthorization(options =>
+        // MVC
+        services
+            .AddResponseCompression(options => { options.EnableForHttps = true; })
+            .AddControllers()
+            .AddNewtonsoftJson(options =>
             {
-                options.AddPolicy("AdministratorsOnly", policy => policy.RequireClaim(ClaimTypes.GroupSid, "Administrators"));
-                options.AddPolicy("EditorsOnly", policy => policy.RequireClaim(ClaimTypes.GroupSid, "Editors"));
+                options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                options.SerializerSettings.TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple;
+                options.SerializerSettings.Converters.Add(new IsoDateTimeConverter());
+                options.SerializerSettings.Converters.Add(new StringEnumConverter());
+                options.SerializerSettings.Converters.Add(new KeyValuePairConverter());
             });
 
-            // API versioning
-            services.AddApiVersioning(options =>
+        // HSTS
+        services.AddHsts(options =>
+        {
+            options.Preload = true;
+            options.MaxAge = TimeSpan.FromDays(Configuration.GetValue<double>("AppConfiguration:HstsMaxAgeInDays"));
+        });
+
+        // Swagger
+        services.AddSwaggerGen(setupAction =>
+        {
+            setupAction.SwaggerDoc(Configuration["Swagger:SpecificationName"], new OpenApiInfo
             {
-                options.ReportApiVersions = true;
-                options.AssumeDefaultVersionWhenUnspecified = true;
-                options.DefaultApiVersion = new ApiVersion(1, 0);
-                options.ApiVersionReader = ApiVersionReader.Combine(
-                    new QueryStringApiVersionReader("api-version", "version", "ver"),
-                    new HeaderApiVersionReader("api-version"));
+                Title = Configuration["Swagger:DocumentTitle"],
+                Version = "1",
+                Description = File.ReadAllText(Configuration["Swagger:DocumentDescription"].Resolve())
+
             });
 
-            // MVC
-            services
-                .AddResponseCompression(options => { options.EnableForHttps = true; })
-                .AddControllers()
-                .AddNewtonsoftJson(options =>
-                {
-                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
-                    options.SerializerSettings.TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple;
-                    options.SerializerSettings.Converters.Add(new IsoDateTimeConverter());
-                    options.SerializerSettings.Converters.Add(new StringEnumConverter());
-                    options.SerializerSettings.Converters.Add(new KeyValuePairConverter());
-                });
-
-            // HSTS
-            services.AddHsts(options =>
+            setupAction.EnableAnnotations();
+            setupAction.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "DHI.Services.Security.WebApi.xml"));
+            setupAction.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
             {
-                options.Preload = true;
-                options.MaxAge = TimeSpan.FromDays(Configuration.GetValue<double>("AppConfiguration:HstsMaxAgeInDays"));
+                Description = "Enter the word 'Bearer' followed by a space and the JWT.",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
             });
 
-            // Swagger
-            services.AddSwaggerGen(setupAction =>
+            setupAction.AddSecurityRequirement(new OpenApiSecurityRequirement()
             {
-                setupAction.SwaggerDoc(Configuration["Swagger:SpecificationName"], new OpenApiInfo
-                {
-                    Title = Configuration["Swagger:DocumentTitle"],
-                    Version = "1",
-                    Description = File.ReadAllText(Configuration["Swagger:DocumentDescription"].Resolve())
-
-                });
-
-                setupAction.EnableAnnotations();
-                setupAction.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "DHI.Services.Security.WebApi.xml"));
-                setupAction.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = "Enter the word 'Bearer' followed by a space and the JWT.",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
-                });
-
-                setupAction.AddSecurityRequirement(new OpenApiSecurityRequirement()
-                {
                 {
                     new OpenApiSecurityScheme
                     {
@@ -134,61 +130,63 @@
                     },
                     new List<string>()
                 }
-                });
             });
-            services.AddSwaggerGenNewtonsoftSupport();
+        });
+        services.AddSwaggerGenNewtonsoftSupport();
 
-            // Pwned passwords
-            services
-                .AddPwnedPasswordHttpClient(minimumFrequencyToConsiderPwned: 1)
-                .AddTransientHttpErrorPolicy(policyBuilder => policyBuilder.RetryAsync(3))
-                .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(2)));
+        // Pwned passwords
+        services
+            .AddPwnedPasswordHttpClient(minimumFrequencyToConsiderPwned: 1)
+            .AddTransientHttpErrorPolicy(policyBuilder => policyBuilder.RetryAsync(3))
+            .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(2)));
 
-            // DHI Domain Services
-#warning In production code, you should use replace the JSON-file based repostiories with for example the PostgreSQL repositories
-            services.AddScoped<IAccountRepository>(_ => new AccountRepository("accounts.json"));
-            services.AddScoped<IMailTemplateRepository>(_ => new MailTemplateRepository("mail-templates.json"));
-            services.AddScoped<IUserGroupRepository>(_ => new UserGroupRepository("user-groups.json"));
-            services.AddScoped<IRefreshTokenRepository>(_ => new RefreshTokenRepository("refresh-tokens.json"));
-            services.AddScoped<IAuthenticationProvider>(_ => new AccountRepository("accounts.json"));
-            services.AddScoped<ILogger>(_ => new JsonLogger("[AppData]log.json".Resolve()));
-        }
+        // DHI Domain Services
+            
+        services.AddScoped<IMailTemplateRepository>(_ => new DHI.Services.Security.WebApi.MailTemplateRepository("mail-templates.json"));
 
-        public virtual void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+#warning TODO: Use PostgreSQL as a service?
+        const string postgreSqlConnectionString = "Server=localhost;Port=5432;Database=DomainServicesCourse;User Id=postgres;Password=Solutions!";
+        services.AddScoped<IAccountRepository>(_ => new DHI.Services.Provider.PostgreSQL.AccountRepository(postgreSqlConnectionString));
+        services.AddScoped<IUserGroupRepository>(_ => new DHI.Services.Provider.PostgreSQL.UserGroupRepository(postgreSqlConnectionString));
+        services.AddScoped<IRefreshTokenRepository>(_ => new DHI.Services.Provider.PostgreSQL.RefreshTokenRepository(postgreSqlConnectionString));
+        services.AddScoped<IAuthenticationProvider>(_ => new DHI.Services.Provider.PostgreSQL.AccountRepository(postgreSqlConnectionString));
+        services.AddScoped<ILogger>(_ => new DHI.Services.Provider.PostgreSQL.Logger(postgreSqlConnectionString));
+    }
+
+    public virtual void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
-
-            app.UseAuthentication();
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            app.UseHttpsRedirection();
-            app.UseSwagger();
-            app.UseSwaggerUI(setupAction =>
-            {
-                var specificationName = Configuration["Swagger:SpecificationName"];
-                setupAction.SwaggerEndpoint($"../swagger/{specificationName}/swagger.json", Configuration["Swagger:DocumentName"]);
-                setupAction.DocExpansion(DocExpansion.None);
-                setupAction.DefaultModelsExpandDepth(-1);
-            });
-            app.UseExceptionHandling();
-            app.UseResponseCompression();
-            app.UseRouting();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-
-            // Set the data directory (App_Data folder) for DHI Domain Services
-            var contentRootPath = Configuration.GetValue("AppConfiguration:ContentRootPath", env.ContentRootPath);
-            AppDomain.CurrentDomain.SetData("DataDirectory", Path.Combine(contentRootPath, "App_Data"));
+            app.UseDeveloperExceptionPage();
         }
+        else
+        {
+            app.UseHsts();
+        }
+
+        app.UseAuthentication();
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+        app.UseHttpsRedirection();
+        app.UseSwagger();
+        app.UseSwaggerUI(setupAction =>
+        {
+            var specificationName = Configuration["Swagger:SpecificationName"];
+            setupAction.SwaggerEndpoint($"../swagger/{specificationName}/swagger.json", Configuration["Swagger:DocumentName"]);
+            setupAction.DocExpansion(DocExpansion.None);
+            setupAction.DefaultModelsExpandDepth(-1);
+        });
+        app.UseExceptionHandling();
+        app.UseResponseCompression();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+
+        // Set the data directory (App_Data folder) for DHI Domain Services
+        var contentRootPath = Configuration.GetValue("AppConfiguration:ContentRootPath", env.ContentRootPath);
+        AppDomain.CurrentDomain.SetData("DataDirectory", Path.Combine(contentRootPath, "App_Data"));
     }
 }
