b3bfceae3903ba608431148720bebb4d98ad23ed
Lars Michael <lars.michael.dk@gmail.com>
2022-07-06 14:41:20 +0200

    description modified to "Workflow Host for DHI Domain Services"

--- Source/WorkflowHostWinService/Install.bat
+++ Source/WorkflowHostWinService/Install.bat
@@ -1,5 +1,5 @@
 cd %~dp0
 @set serviceName="DHI Workflow Host"
 sc.exe create %serviceName% start=delayed-auto binpath="%~dp0WorkflowHostWinService.exe" 
-sc.exe description %serviceName% "DHI Workflow Host" 
+sc.exe description %serviceName% "Workflow Host for DHI Domain Services" 
 sc.exe start %serviceName%
