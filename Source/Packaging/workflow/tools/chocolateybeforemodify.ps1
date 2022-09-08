bcf0bf0db131b0c0d3791bff001d147c2ce8f1e7
dyk dhi <dyk@ dhi>
2022-09-08 13:58:48 -0600

    workflow packaging updates

---
+++ Source/Packaging/workflow/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,27 @@
+﻿# This runs in 0.9.10+ before upgrade and uninstall.
+# Use this file to do things like stop services prior to upgrade or uninstall.
+# NOTE: It is an anti-pattern to call chocolateyUninstall.ps1 from here. If you
+#  need to uninstall an MSI prior to upgrade, put the functionality in this
+#  file without calling the uninstall script. Make it idempotent in the
+#  uninstall script so that it doesn't fail when it is already uninstalled.
+# NOTE: For upgrades - like the uninstall script, this script always runs from 
+#  the currently installed version, not from the new upgraded package version.
+
+
+$validExitCodes= @(0)
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+#
+# run uninstallation utilities
+#
+$statementsToRun = "NET STOP ""DHI Workflow Host"""
+Start-ChocolateyProcessAsAdmin $statementsToRun -validExitCodes $validExitCodes
+
+$statementsToRun = "& ""C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe"" /u ""$toolsDir\WorkflowHostWinService.exe"""
+Start-ChocolateyProcessAsAdmin $statementsToRun -validExitCodes $validExitCodes
+
+#
+# remove files unzipped during install
+#
+$statementsToRun = "Remove-Item $toolsDir\* -Recurse -Force"
+Start-ChocolateyProcessAsAdmin $statementsToRun -validExitCodes $validExitCodes
