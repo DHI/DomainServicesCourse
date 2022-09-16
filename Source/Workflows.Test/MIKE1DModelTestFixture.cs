e254d200958d494027c9e2edf42332d8d4e792d9
Franz <post@franzthomsen.dk>
2022-09-16 07:14:23 +0200

    wip

--- Source/Workflows.Test/MIKE1DModelTestFixture.cs
+++ Source/Workflows.Test/MIKE1DModelTestFixture.cs
@@ -10,10 +10,13 @@ public class MIKE1DModelTestFixture : IDisposable
         Root = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
         var masterFolder = Path.Combine(Root, "Master");
         Directory.CreateDirectory(masterFolder);
-        foreach (var file in Directory.GetFiles("..\\..\\..\\..\\..\\..\\Models\\MIKE1D\\Master"))
+        foreach (var file in Directory.GetFiles("..\\..\\..\\..\\..\\..\\Models\\MIKE1D\\Master")) 
+        {
             File.Copy(file, Path.Combine(masterFolder, Path.GetFileName(file)));
+        }
 
         File.Copy("..\\..\\..\\..\\..\\..\\Models\\MIKE1D\\TransferTimeSeries.xlsx", Path.Combine(Root, "TransferTimeSeries.xlsx"));
+        File.Copy("..\\..\\..\\..\\..\\..\\Models\\MIKE1D\\BuildTimeSeries.xlsx", Path.Combine(Root, "BuildTimeSeries.xlsx"));
     }
 
     public string Root { get; }
